054a45e6e215d36cc1d82d9b6e8b6f95e0518a94
Gabriel Dos Santos Silva <gabrieldos55570000@gmail.com>
2022-07-22 16:04:31 -0300

    integração com o localStorage concluído

--- pokedex15/src/components/pages/Pokedex/Pokedex.js
+++ pokedex15/src/components/pages/Pokedex/Pokedex.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { goBack } from '../../Routes/coordinator'
 
 export default function Pokedex() {
+
+  const [pokes, setPokes] = useState([])
+
   const navigate = useNavigate()
 
+  useEffect(()=> {
+    pegaPokemon()
+  },[])
+
+  const pegaPokemon = ()=>{
+    if (localStorage.hasOwnProperty("listaPokes")) {
+      const poke = JSON.parse(localStorage.getItem("listaPokes"))
+      setPokes( poke )
+    }
+  }
+console.log(pokes)
   return (
     <div>
       <h1>Pokedex</h1>
-      <button onClick={() => goBack(navigate)}>Voltar</button>
+
+
     </div>
   )
 }
